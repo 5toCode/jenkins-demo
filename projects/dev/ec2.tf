62155fc248f3524b22af1de8af926bdac90ba3e1
5toCode <mike70@gmail.com>
2022-03-24 11:45:39 -0400

    change sec groups

--- projects/dev/ec2.tf
+++ projects/dev/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "jenkins-server" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
+  key_name      = "jenkins-kp"
 
   vpc_security_group_ids = ["${aws_security_group.jenkins-demo-sg.id}"]
 
@@ -17,7 +18,7 @@ resource "aws_security_group" "jenkins-demo-sg" {
 
   ingress {
     self      = false
-    from_port = 0
+    from_port = 22
     to_port   = 22
     protocol  = "tcp"
 
@@ -27,7 +28,7 @@ resource "aws_security_group" "jenkins-demo-sg" {
   }
   ingress {
     self      = false
-    from_port = 0
+    from_port = 80
     to_port   = 80
     protocol  = "tcp"
 
@@ -37,7 +38,7 @@ resource "aws_security_group" "jenkins-demo-sg" {
   }
   ingress {
     self      = false
-    from_port = 0
+    from_port = 8080
     to_port   = 8080
     protocol  = "tcp"
 
