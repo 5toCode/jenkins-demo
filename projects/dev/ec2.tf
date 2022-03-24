2f3946a09f709591088d4e4b4c3bda792535cdaf
5toCode <mike70@gmail.com>
2022-03-24 08:19:21 -0400

    add tf files

---
+++ projects/dev/ec2.tf
@@ -0,0 +1,48 @@
+resource "aws_instance" "jenkins-server" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["${aws_security_group.jenkins-demo-sg.id}"]
+
+  tags = {
+    Name = "jenkins-server"
+  }
+}
+
+resource "aws_security_group" "jenkins-demo-sg" {
+
+  name        = "jenkins-demo-sg"
+  description = "Security group for jenkins demo"
+  vpc_id      = "vpc-00e7e47a"
+
+  ingress {
+    self      = false
+    from_port = 0
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  ingress {
+    self      = false
+    from_port = 0
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+  ingress {
+    self      = false
+    from_port = 0
+    to_port   = 8080
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+}
---
+++ projects/dev/variables.tf
@@ -0,0 +1,8 @@
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
